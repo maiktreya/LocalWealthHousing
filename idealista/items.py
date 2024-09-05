4ef62c1b193ef231ce7539c5982b84928d9c6f44
maiktreya <miguel.garcia.duch@gmail.com>
2024-09-05 13:29:12 +0200

    last

--- idealista/items.py
+++ idealista/items.py
@@ -9,21 +9,13 @@ import scrapy
 
 
 class IdealistaItem(scrapy.Item):
-    title = scrapy.Field()
+    #Matching variables of every flat to be scrapped
+    #id_idealista = scrapy.Field()
+    date = scrapy.Field()
     link = scrapy.Field()
     price = scrapy.Field()
-    rooms = scrapy.Field()
-    m2 = scrapy.Field()
-    description = scrapy.Field()
-    costs_per_month = scrapy.Field()
     address = scrapy.Field()
-    addons = scrapy.Field()
-
-
-class SellItem(IdealistaItem):
-    drop_price = scrapy.Field()
-    price_per_m2 = scrapy.Field()
-
-
-class RentItem(IdealistaItem):
-    rent = scrapy.Field()
+    sqft_m2 = scrapy.Field()
+    rooms = scrapy.Field()
+    discount = scrapy.Field()
+    floor_elevator = scrapy.Field()
