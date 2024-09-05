bba996774e3742d6fcdd859029e373210da44cbf
maiktreya <miguel.garcia.duch@gmail.com>
2024-09-05 09:54:19 +0200

    last

--- idealista/spiders/segovia_capital_spider.py
+++ idealista/spiders/segovia_capital_spider.py
@@ -1,8 +1,8 @@
-from idealista.spiders.coreDev import Core
-
-
-class MadridViviendaSpider(Core):
-    name = "segovia-segovia"
-
-    def __init__(self):
-        super(MadridViviendaSpider, self).__init__(self.name, "alquiler", "viviendas")
+# from idealista.spiders.core import Core
+# 
+# 
+# class MadridViviendaSpider(Core):
+#     name = "segovia-segovia"
+# 
+#     def __init__(self):
+#         super(MadridViviendaSpider, self).__init__(self.name, "alquiler", "viviendas")
