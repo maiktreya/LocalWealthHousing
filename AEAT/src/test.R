53d472b5e8882b44a52f2af6e84d983970d71312
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-08 01:57:55 +0200

    aq

--- AEAT/src/test.R
+++ AEAT/src/test.R
@@ -3,6 +3,11 @@ library("magrittr")
 library("data.table")
 library("survey")
 iden21 <- fread("AEAT/data/ief2021/iden.gz")
+par150 <- fread("AEAT/data/ief2021/par150.gz")
+pat <- fread("AEAT/data/ief2021/pat.gz")
+renta <- fread("AEAT/data/ief2021/renta.gz")
+tipodec <- fread("AEAT/data/ief2021/tipodec.gz")
+
 
 # Example auxiliary data for "CityX"
 city_population <- 100000  # Replace with actual population value
