0ea61bc519831e2da66e6f237107839c82100522
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-08 14:51:43 +0200

    dsgda

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
 # Activate virtual environment
 source /home/other/dev/github/LocalWealthHousing/env/bin/activate
 
-# Run scraper for the first URL
-echo "Running scraper for Segovia Sale..."
+# Run Airbnb scrapers
+echo "Running Airbnb Short-Term Scraper..."
+/home/other/dev/github/LocalWealthHousing/env/bin/python /home/other/dev/github/LocalWealthHousing/scrapping/src/airbnb/airbnb_apify_short.py && \
+echo "Airbnb Short-Term Scraper finished. Running Medium-Term Scraper..."
+
+echo "Running Airbnb Medium-Term Scraper..."
+/home/other/dev/github/LocalWealthHousing/env/bin/python /home/other/dev/github/LocalWealthHousing/scrapping/src/airbnb/airbnb_apify_medium.py && \
+echo "Airbnb Medium-Term Scraper finished. Running Long-Term Scraper..."
+
+echo "Running Airbnb Long-Term Scraper..."
+/home/other/dev/github/LocalWealthHousing/env/bin/python /home/other/dev/github/LocalWealthHousing/scrapping/src/airbnb/airbnb_apify_long.py
+echo "Airbnb Long-Term Scraper finished."
+
+# Now run Idealista scrapers
+echo "Running Idealista Segovia Sale Scraper..."
 /home/other/dev/github/LocalWealthHousing/env/bin/python /home/other/dev/github/LocalWealthHousing/scrapping/src/idealista/idealista_httpx.ori.py --url "https://www.idealista.com/venta-viviendas/segovia-segovia/" --delay 5
 echo "Finished scraping Segovia Sale. Waiting 5 minutes..."
 
@@ -11,6 +25,6 @@ echo "Finished scraping Segovia Sale. Waiting 5 minutes..."
 sleep 300
 
 # Run scraper for the second URL
-echo "Running scraper for Segovia Rent..."
+echo "Running Idealista Segovia Rent Scraper..."
 /home/other/dev/github/LocalWealthHousing/env/bin/python /home/other/dev/github/LocalWealthHousing/scrapping/src/idealista/idealista_httpx.ori.py --url "https://www.idealista.com/alquiler-viviendas/segovia-segovia/" --delay 5
 echo "Finished scraping Segovia Rent."
