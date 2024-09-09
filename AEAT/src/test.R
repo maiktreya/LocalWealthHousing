5f215c0e863cfb123994a4ae65a0f9ad0cb360ac
maiktreya <miguel.garcia.duch@gmail.com>
2024-09-09 12:45:50 +0200

    last

--- AEAT/src/test.R
+++ AEAT/src/test.R
@@ -15,23 +15,23 @@ dt <- fread("LocalWealthHousing/AEAT/data/IEF-2021-new.gz")
 dt[, (selected_columns) := lapply(.SD, function(x) ifelse(is.na(x), 0, x)), .SDcols = selected_columns]
 
 # Main data transformation TABLA[ filter_rows , select_columns  , group_by ]
-    dt[TRAMO == "N", TRAMO := 8][, TRAMO := as.numeric(TRAMO)]
+dt[TRAMO == "N", TRAMO := 8][, TRAMO := as.numeric(TRAMO)]
 
-    dt <- dt[TIPODEC %in% c("T1", "T21") & !is.na(FACTORCAL),
-        .(
-            RENTAD = sum(RENTAD),
-            TRAMO = mean(TRAMO),
-            RENTA_ALQ = sum(RENTA_ALQ),
-            PAR150 = sum(PAR150),
-            PATINMO = sum(PATINMO),
-            FACTORCAL = mean(FACTORCAL),
-            CCAA = mean(CCAA),
-            PROV = mean(PROV),
-            MUNI = mean(MUNI)
-        ),
-        by = .(reference = get(ref_unit))
-    ]
-    setnames(dt, "reference", as.character(ref_unit))
+dt <- dt[TIPODEC %in% c("T1", "T21") & !is.na(FACTORCAL),
+    .(
+        RENTAD = sum(RENTAD),
+        TRAMO = mean(TRAMO),
+        RENTA_ALQ = sum(RENTA_ALQ),
+        PAR150 = sum(PAR150),
+        PATINMO = sum(PATINMO),
+        FACTORCAL = mean(FACTORCAL),
+        CCAA = mean(CCAA),
+        PROV = mean(PROV),
+        MUNI = mean(MUNI)
+    ),
+    by = .(reference = get(ref_unit))
+]
+setnames(dt, "reference", as.character(ref_unit))
 
 # identificar municipios a analizar
 dt[CCAA == "7" & PROV == "40" & MUNI == "194", segovia := 1]
@@ -46,4 +46,9 @@ dt[, PROPIETARIO_SIN := 0][PATINMO > 0 & CASERO == 0, PROPIETARIO_SIN := 1][, PR
 dt[, INQUILINO := 1][PROPIETARIO_SIN == 1, INQUILINO := 0][CASERO == 1, INQUILINO := 0][, INQUILINO := factor(INQUILINO)]
 dt[, RENTAD_NOAL := 0][, RENTAD_NOAL := RENTAD - RENTA_ALQ]
 
-
+# construir la muestra estadística de fase 1
+dt_sv <- svydesign(
+    ids = ~1,
+    data = dt,
+    weights = dt$FACTORCAL
+) # muestra con coeficientes de elevación
