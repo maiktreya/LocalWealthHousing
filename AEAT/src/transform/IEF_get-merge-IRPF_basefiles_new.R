5e93f285d2285a08bd3f3c1bc82acc88e8c2e230
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-09 21:12:28 +0200

    git config pull.rebase false

--- AEAT/src/transform/IEF_get-merge-IRPF_basefiles_new.R
+++ AEAT/src/transform/IEF_get-merge-IRPF_basefiles_new.R
@@ -19,9 +19,9 @@ colnames(iden) <- c("IDENPER", "IDENHOG", "CCAA", "PROV", "MUNI", "TRAMO", "FACT
 start_iden2 <- c(1, 12, 13, 17) # Starting positions references
 end_iden2 <- c(11, 12, 16, 19) # Ending positions references
 col_iden2 <- fwf_positions(start = start_iden2, end = end_iden2) # Use fwf_positions to define column positions
-iden2 <- read_fwf(paste0("AEAT/data/original/1_IDEN", sel_year, ".txt"), col_positions = col_iden2) %>% data.table()
+iden2 <- read_fwf(paste0("AEAT/data/original/10_IDEN", ".txt"), col_positions = col_iden2) %>% data.table()
 colnames(iden2) <- c("IDENPER", "SEXO", "ANONAC", "PAISNAC")
 
 # FUSIONAR EN FICHERO CON INDETIFICACIÓN COMPLETA
-dt <- merge(iden, IDEN2, by = c("IDENPER"))
-fwrite(iden, paste0("AEAT/data/ief2021/", sel_year, "_iden.gz"))
+dt <- merge(iden, iden2, by = c("IDENPER"))
+fwrite(dt, paste0("AEAT/data/ief2021/", sel_year, "_iden.gz"))
