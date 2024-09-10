2e2ac8ea7af49e5684351b06fcc627dca728f7e1
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-10 05:43:11 +0200

    ggg

--- scrapping/src/airbnb/airbnb_scraper.py
+++ scrapping/src/airbnb/airbnb_scraper.py
@@ -7,8 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import time
+from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import logging
 import os
