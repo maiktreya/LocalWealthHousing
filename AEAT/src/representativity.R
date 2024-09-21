05a76ae78a4d88eb27bbdabd98a93cc181b2b118
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-21 22:47:15 +0200

    rwe

--- AEAT/src/representativity.R
+++ AEAT/src/representativity.R
@@ -16,4 +16,5 @@ subsample <- subset(survey_design, MUESTRA == 1)
 # performe representativness test on key variables with known distributional values
 test_rep1 <- svyttest(I(RENTAD - 34272) ~ 0, subsample) %>% print()
 test_rep2 <- svyttest(I(RENTAB - 41235) ~ 0, subsample) %>% print()
-confint
+confint(test_rep1, level = 0.95) %>% print()
+confint(test_rep2, level = 0.95) %>% print()
