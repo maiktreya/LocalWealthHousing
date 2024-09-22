ad39662d2dd63875a0b832abb04b50052c69cdb0
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-22 17:01:44 +0200

    ewt

--- AEAT/src/etl_pipe.R
+++ AEAT/src/etl_pipe.R
@@ -4,25 +4,32 @@ library("data.table")
 rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 
 # Use either IDENPER for personal or IDENHOG for household level
+
 sel_year <- 2021
 ref_unit <- "IDENPER"
 sel_cols <- c("RENTAD", "RENTAB", "RENTA_ALQ", "PATINMO", "PAR150")
 
 # Import chosen dataframe (change string according to the data file path)
+
 dt <- fread(paste0("AEAT/data/IEF-", sel_year, "-new.gz")) # from IEAT IRPF sample
 
 # Replace NA values with 0 in selected columns
+
 dt[, (sel_cols) := lapply(.SD, function(x) ifelse(is.na(x), 0, x)), .SDcols = sel_cols]
 
 # Main data transformation: Filter rows and summarize
+
 dt[TRAMO == "N", TRAMO := 8][, TRAMO := as.numeric(TRAMO)]
 
 # Identify towns to analyze
+
 dt[, MUESTRA := 0] # add a column for the subsample identifier
 dt[CCAA == "7" & PROV == "40" & MUNI == "194", MUESTRA := 1] # segovia
 dt[CCAA == "7" & PROV == "40" & MUNI == "112", MUESTRA := 2] # lastrilla
 dt[CCAA == "7" & PROV == "40" & MUNI == "906", MUESTRA := 3] # sancris
 dt[CCAA == "7" & PROV == "40" & MUNI == "155", MUESTRA := 4] # palazuelos
+
+# tidy dt for the given reference unit through in-place vectorized operations
 
 dt <- dt[!is.na(FACTORCAL),
     .(
@@ -46,12 +53,14 @@ dt <- dt[!is.na(FACTORCAL),
 ]
 
 # Rename the reference column to match 'ref_unit'
+
 ifelse(ref_unit == "IDENHOG",
     dt <- dt[, -c("reference")],
     setnames(dt, "reference", as.character(ref_unit))
 )
 
 # Define any new categorical variable before setting the survey object
+
 dt[, TENENCIA := "INQUILINA"]
 dt[PAR150 > 0, TENENCIA := "CASERO"]
 dt[PATINMO > 0 & TENENCIA != "CASERO", TENENCIA := "PROPIETARIO"]
