5335e71a45674622acd75b9396476c6571b24666
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-22 16:30:12 +0200

    ewfw

--- AEAT/src/representativity.R
+++ AEAT/src/representativity.R
@@ -7,8 +7,8 @@ source("AEAT/src/etl_pipe.R")
 # import population distributional values
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 get_col <- colnames(pop_stats)[colnames(pop_stats) %like% tolower(ref_unit)]
-RNpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RN_", tolower(ref_unit))) ]
-RBpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RB_", tolower(ref_unit))) ]
+RNpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
+RBpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RB_", tolower(ref_unit)))]
 
 # Create the survey design object with the initial weights
 survey_design <- svydesign(
@@ -19,7 +19,7 @@ survey_design <- svydesign(
 subsample <- subset(survey_design, MUESTRA == 1)
 
 # performe representativness test on key variables with known distributional values
-test_rep1 <- svyttest(I(RENTAD - 34272) ~ 0, subsample) %>% print()
-test_rep2 <- svyttest(I(RENTAB - 41235) ~ 0, subsample) %>% print()
+test_rep1 <- svyttest(I(RENTAD - RNpop) ~ 0, subsample) %>% print()
+test_rep2 <- svyttest(I(RENTAB - RBpop) ~ 0, subsample) %>% print()
 test_conf1 <- confint(test_rep1, level = 0.95) %>% print()
 test_conf2 <- confint(test_rep2, level = 0.95) %>% print()
