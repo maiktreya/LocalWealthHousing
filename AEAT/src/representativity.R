71277a3893ddc9d4e443b555050cde0265659517
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-22 16:38:50 +0200

    sdgds

--- AEAT/src/representativity.R
+++ AEAT/src/representativity.R
@@ -1,16 +1,19 @@
 # Obtain t-statisctics for representative mean forAEAT subsample
+
 library(survey)
 library("magrittr")
 rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 source("AEAT/src/etl_pipe.R")
 
 # import population distributional values
+
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 get_col <- colnames(pop_stats)[colnames(pop_stats) %like% tolower(ref_unit)]
 RNpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
 RBpop <- pop_stats[muni == "segovia" & year == sel_year, get(paste0("RB_", tolower(ref_unit)))]
 
 # Create the survey design object with the initial weights
+
 survey_design <- svydesign(
     ids = ~1,
     data = dt,
@@ -19,7 +22,16 @@ survey_design <- svydesign(
 subsample <- subset(survey_design, MUESTRA == 1)
 
 # performe representativness test on key variables with known distributional values
+
+RNmean <- svymean(~RENTAD, subsample)
+RBmean <- svymean(~RENTAB, subsample)
+
 test_rep1 <- svyttest(I(RENTAD - RNpop) ~ 0, subsample) %>% print()
 test_rep2 <- svyttest(I(RENTAB - RBpop) ~ 0, subsample) %>% print()
+
 test_conf1 <- confint(test_rep1, level = 0.95) %>% print()
 test_conf2 <- confint(test_rep2, level = 0.95) %>% print()
+
+net_vals <- data.table(pop = RNpop, mean = RNmean, dif = (RNpop - RNmean) / RNpop)
+gross_vals <- data.table(pop = RBpop, mean = RBmean, dif = (RBpop - RBmean) / RBpop)
+results <- rbind(net_vals, gross_vals) %>% print()
