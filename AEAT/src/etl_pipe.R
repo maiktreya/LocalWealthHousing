99b389d77a346acfa23d22c8420028ef4286757b
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-22 16:41:10 +0200

    DDD

--- AEAT/src/etl_pipe.R
+++ AEAT/src/etl_pipe.R
@@ -4,8 +4,8 @@ library("data.table")
 rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 
 # Use either IDENPER for personal or IDENHOG for household level
-sel_year <- 2016
-ref_unit <- "IDENHOG"
+sel_year <- 2021
+ref_unit <- "IDENPER"
 selected_columns <- c("RENTAD", "RENTAB", "RENTA_ALQ", "PATINMO", "PAR150")
 
 # Import chosen dataframe (change string according to the data file path)
