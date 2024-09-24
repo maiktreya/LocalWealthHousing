692b81b1461e6ba94893e3481c1947c87eeddd72
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-25 00:08:27 +0200

    fgg

--- AEAT/src/transform/IEF_get-mainfiles -dev.R
+++ AEAT/src/transform/IEF_get-mainfiles -dev.R
@@ -10,7 +10,9 @@ rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 sel_year <- 2021
 dt <- fread(paste0("AEAT/data/IEF-", sel_year, "-new-old.gz")) # from IEAT IRPF sample
 
+
 # IDENTIFICADORES Y PESOS fichero 1_IDEN.txt
+
 start_iden <- c(1, 12, 23, 25, 27, 35, 36, 132) # Starting positions references
 end_iden <- c(11, 22, 24, 26, 29, 35, 55, 141) # Ending positions references
 col_iden <- fwf_positions(start = start_iden, end = end_iden) # Use fwf_positions to define column positions
@@ -18,9 +20,30 @@ iden <- read_fwf(paste0("AEAT/data/original/1_IDEN", sel_year, ".txt"), col_posi
 colnames(iden) <- c("IDENPER", "IDENHOG", "CCAA", "PROV", "MUNI", "TRAMO", "FACTORCAL", "SECCION")
 
 
-# Merge both datasets based on IDs
-dt <- merge(dt, iden, by = c("IDENPER", "IDENHOG"))
+# PAR150 REDUCCIÓN ALQUILER VIVIENDA solo comprobado para 2021!
 
+if (sel_year == 2021) {
+    start_dt150 <- c(1, 12, 23, 154, 962)
+    end_dt150 <- c(11, 22, 33, 173, 981)
+    col_dt150 <- fwf_positions(start = start_dt150, end = end_dt150) # Use fwf_positions to define column positions
+    dt150 <- read_fwf(paste0("AEAT/data/original/8_IRPF", sel_year, "_RRII.txt"), col_positions = col_dt150) %>% data.table()
+    colnames(dt150) <- c("IDENPER", "IDENHOG", "REFCAT", "INCALQ", "PAR150i")
+}
+if (sel_year == 2016) {
+    start_p150 <- c(1, 12, 23, 71, 203)
+    end_p150 <- c(11, 22, 33, 82, 214)
+    col_p150 <- fwf_positions(start = start_p150, end = end_p150) # Use fwf_positions to define column positions
+    dt150 <- read_fwf(paste0("AEAT/data/original/8_IRPF", sel_year, "_RRII.txt"), col_positions = col_p150) %>% data.table()
+    colnames(dt150) <- c("IDENPER", "IDENHOG", "REFCAT", "INCALQ", "PAR150i")
+}
+
+# Merge both datasets based on IDs
+
+dt <- merge(dt, iden, by = c("IDENPER", "IDENHOG"))
+dt <- merge(dt, dt150, by = c("IDENPER", "IDENHOG"))
+
+
+print(nrow(dt))
 # export the results
 
 fwrite(dt, paste0("AEAT/data/IEF-", sel_year, "-new.gz")) # exportar objeto preparado
