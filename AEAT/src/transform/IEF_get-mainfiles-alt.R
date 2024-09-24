0f8b4aea6638d6a735ade0fb0eccbf1851a944fd
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-24 20:44:09 +0200

    adsf

--- AEAT/src/transform/IEF_get-mainfiles-alt.R
+++ AEAT/src/transform/IEF_get-mainfiles-alt.R
@@ -1,29 +1,24 @@
-# Fichero principal 2021
+# Piping AEAT anual realease files into a single efficient data.table matrix stored as gz file
 
+library(magrittr)
+library(readr)
 library(data.table)
 rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 
-sel_year <- 2021
-dt <- fread("AEAT/data/ief2021/par150.gz")
+sel_year <- 2016
+################################
 
-dt <- dt[, # group todas las declaraciones por persona. PAR150: ded. por alq. de viv.)
-    .(
-        IDENHOG = mean(IDENHOG),
-        PAR150 = sum(PAR150)
-    ),
-    by = IDENPER
-]
+# IDENTIFICADORES Y PESOS fichero 1_IDEN.txt
+start_iden <- c(1, 12, 23, 25, 27, 35, 36, 132) # Starting positions references
+end_iden <- c(11, 22, 24, 26, 29, 35, 55, 141) # Ending positions references
+col_iden <- fwf_positions(start = start_iden, end = end_iden) # Use fwf_positions to define column positions
+iden <- read_fwf(paste0("AEAT/data/original/1_IDEN", sel_year, ".txt"), col_positions = col_iden) %>% data.table()
+colnames(iden) <- c("IDENPER", "IDENHOG", "CCAA", "PROV", "MUNI", "TRAMO", "FACTORCAL", "SECCION")
 
-dt_tipo <- fread(paste0("AEAT/data/ief", sel_year, "/tipodec.gz")) # tipo de declaración already grouped by IDENPER
-dt <- merge(dt, dt_tipo, by = c("IDENPER", "IDENHOG"))
 
-dt_iden <- fread(paste0("AEAT/data/ief", sel_year, "/2021_iden.gz")) #  pesos muestrales
-dt <- merge(dt, dt_iden, by = c("IDENPER", "IDENHOG"))
-
-dt_renta <- fread(paste0("AEAT/data/ief", sel_year, "/renta.gz")) # renta y alquiler
-dt <- merge(dt, dt_renta, by = c("IDENPER", "IDENHOG"))
-
-dt_pat <- fread(paste0("AEAT/data/ief", sel_year, "/pat.gz")) # patrimonio inmobiliario
-dt <- merge(dt, dt_pat, by = c("IDENPER", "IDENHOG"))
-
-fwrite(dt, paste0("AEAT/data/IEF-", sel_year, "-new.gz")) # exportar objeto preparado
+# RENTA TIPO DECLARANTE
+start_dt_dec <- c(1, 12, 23, 27, 29, 1491, 1011)
+end_dt_dec <- c(11, 22, 25, 27, 32, 1502, 1022)
+col_dt_dec <- fwf_positions(start = start_dt_dec, end = end_dt_dec) # Use fwf_positions to define column positions
+dt_dec <- read_fwf(paste0("AEAT/data/original/4_IRPF", sel_year, ".txt"), col_positions = col_dt_dec) %>% data.table()
+colnames(dt_dec) <- c("IDENPER", "IDENHOG", "TIPODEC", "SEXO", "ANONAC", "PAR150")
