b151a7fb4d90dc6890529f91346cdcf656545ebc
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-26 00:05:02 +0200

    we

---
+++ AEAT/src/tests/representativity-ori.R
@@ -0,0 +1,42 @@
+# Obtain t-statisctics for representative mean for AEAT subsample
+
+library(survey)
+library("magrittr")
+rm(list = ls()) # clean enviroment to avoid ram bottlenecks
+
+# import microdata & population distributional values
+
+source("AEAT/src/etl_pipe.R")
+pop_stats <- fread("AEAT/data/pop-stats.csv")
+municipios <- cbind(mun = c("segovia", "lastrilla", "sancris", "palazuelos"), ind = c(1:4)) %>% data.table()
+
+# define city subsample and variables to analyze
+
+city <- "segovia"
+get_col <- colnames(pop_stats)[colnames(pop_stats) %like% tolower(ref_unit)]
+RNpop <- pop_stats[muni == city & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
+RBpop <- pop_stats[muni == city & year == sel_year, get(paste0("RB_", tolower(ref_unit)))]
+
+# Create the survey design object with the initial weights
+
+survey_design <- svydesign(
+    ids = ~1,
+    data = dt,
+    weights = dt$FACTORCAL
+)
+subsample <- subset(survey_design, MUESTRA == municipios[mun == city]$ind)
+
+# performe representativness test on key variables with known distributional values
+
+RNmean <- svymean(~RENTAD, subsample)
+RBmean <- svymean(~RENTAB, subsample)
+
+test_rep1 <- svyttest(I(RENTAD - RNpop) ~ 0, subsample) %>% print()
+test_rep2 <- svyttest(I(RENTAB - RBpop) ~ 0, subsample) %>% print()
+
+test_conf1 <- confint(test_rep1, level = 0.95) %>% print()
+test_conf2 <- confint(test_rep2, level = 0.95) %>% print()
+
+net_vals <- data.table(pop = RNpop, mean = RNmean, dif = (RNpop - RNmean) / RNpop)
+gross_vals <- data.table(pop = RBpop, mean = RBmean, dif = (RBpop - RBmean) / RBpop)
+results <- rbind(net_vals, gross_vals) %>% print()
