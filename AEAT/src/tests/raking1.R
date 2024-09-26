f14eda9ca9d1450a8479fa40d7fcfbd680f05f89
maiktreya <miguel.garcia.duch@gmail.com>
2024-09-26 16:17:22 +0200

    thgf

--- AEAT/src/tests/raking1.R
+++ AEAT/src/tests/raking1.R
@@ -5,19 +5,20 @@ library("data.table")
 library("survey")
 rm(list = ls()) # Clean environment to avoid RAM bottlenecks
 
-ref_survey <- "IEF" # Either IEF or EFF
-ref_unit <- "IDENPER" # Use either IDENPER for personal or IDENHOG for household levels
-selected_columns <- c("RENTAD", "RENTAB", "RENTA_ALQ", "PATINMO")
-
-# Import chosen dataframe (change string according to the data file path)
-dt <- fread("LocalWealthHousing/AEAT/data/IEF-2021-new.gz") # from IEAT IRPF sample
-fr <- fread("LocalWealthHousing/AEAT/data/ief2021/pob-segovia.csv") # from INE population distribution
+source("AEAT/src/transform/etl_pipe.R")
+city <- "Segovia"
+represet <- "!is.na(FACTORCAL)" # población
+represet2 <- 'TIPODEC %in% c("T1", "T21") & !is.na(FACTORCAL)' # declarantes de renta
+sel_year <- 2016
+ref_unit <- "IDENHOG"
+dt <- get_wave(sel_year = sel_year, ref_unit = ref_unit, represet = represet)
+fr <- fread("AEAT/data/madrid-age.csv") # from INE population distribution
 
 # Process the population data (assuming the first row contains overall totals)
 sex <- fr[1, .(age, segoT, segoH, segoM)]
 fr <- fr[-1, .(age, segoT, segoH, segoM)]
 
-fr[, age_group := cut(as.numeric(age), breaks = seq(0, 100, by = 10), right = FALSE)]
+fr[, age_group := cut(as.numeric(age), breaks = seq(0, 100, by = 5), right = FALSE)]
 
 # Summarize the population by age group
 age_distribution <- fr[, .(Freq = sum(segoT) / sum(sex$segoT)), by = age_group]
@@ -26,37 +27,6 @@ gender_distribution <- data.table(
     gender = c("male", "female"),
     Freq = c(sex$segoH / sex$segoT, sex$segoM / sex$segoT)
 )
-
-# Replace NA values with 0 in selected columns
-dt[, (selected_columns) := lapply(.SD, function(x) ifelse(is.na(x), 0, x)), .SDcols = selected_columns]
-
-# Main data transformation: Filter rows and summarize
-dt[TRAMO == "N", TRAMO := 8][, TRAMO := as.numeric(TRAMO)]
-
-# Identify towns to analyze
-dt[CCAA == "7" & PROV == "40" & MUNI == "194", segovia := 1]
-
-dt <- dt[TIPODEC %in% c("T1", "T21") & !is.na(FACTORCAL),
-    .(
-        segovia = mean(segovia),
-        SEXO = mean(SEXO), # 1 = Male, 2 = Female
-        age = 2022 - mean(ANONAC), # Calculate age
-        RENTAB = sum(RENTAB),
-        RENTAD = sum(RENTAD),
-        TRAMO = mean(TRAMO),
-        RENTA_ALQ = sum(RENTA_ALQ),
-        PAR150 = sum(PAR150),
-        PATINMO = sum(PATINMO),
-        FACTORCAL = mean(FACTORCAL),
-        CCAA = mean(CCAA),
-        PROV = mean(PROV),
-        MUNI = mean(MUNI)
-    ),
-    by = .(reference = get(ref_unit))
-]
-
-# Rename the reference column to match 'ref_unit'
-setnames(dt, "reference", as.character(ref_unit))
 
 # Ensure gender is categorical and matches the population margin
 dt[, gender := ifelse(SEXO == 1, "male", "female")]
@@ -70,6 +40,30 @@ dt <- dt[!is.na(age_group)]
 # Check the structure of the age and gender variables
 table(dt$age_group)
 table(dt$gender)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Create the survey design object with the initial weights
 survey_design <- svydesign(
