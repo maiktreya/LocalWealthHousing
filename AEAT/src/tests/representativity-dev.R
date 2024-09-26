e4d42859fa9c0623da5c73844ba17d9305372f89
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-26 21:40:20 +0200

    GH

--- AEAT/src/tests/representativity-dev.R
+++ AEAT/src/tests/representativity-dev.R
@@ -9,7 +9,7 @@ source("AEAT/src/transform/etl_pipe.R")
 # define city subsample and variables to analyze
 city <- "segovia"
 represet <- "!is.na(FACTORCAL)" # población
-sel_year <- 2016
+sel_year <- 2021
 ref_unit <- "IDENPER"
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 RNpop <- pop_stats[muni == city & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
