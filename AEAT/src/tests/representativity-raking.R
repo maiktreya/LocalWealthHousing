a06a07eb02d18598f7a72cabeb0e02c591c5864b
il    iciuv <miguel.garcia.duch@gmail.com>
2024-09-27 16:16:24 +0200

    rewrew

--- AEAT/src/tests/representativity-raking.R
+++ AEAT/src/tests/representativity-raking.R
@@ -12,11 +12,9 @@ city <- "madrid"
 represet <- "!is.na(FACTORCAL)" # población
 sel_year <- 2016
 ref_unit <- "IDENPER"
-age <- fread("AEAT/data/madrid-age.csv")
-sex <- fread("AEAT/data/madrid-sex.csv")
+age_vector <- fread("AEAT/data/madrid-age-freq.csv")[, .(age_group, Freq=get(paste0("freq",sel_year)))]
+sex_vector <- fread("AEAT/data/madrid-sex-freq.csv")[, .(gender, Freq=get(paste0("freq",sel_year)))]
 dt <- get_wave(sel_year = sel_year, ref_unit = ref_unit, represet = represet)
-# Example age vector (replace this with your actual data)
-dt[, gender := "female"][SEXO == 1, gender := "male"]
 
 # Create a new age_group based on broader 20-year intervals, with the last one open-ended
 dt[, age_group := cut(
@@ -26,20 +24,13 @@ dt[, age_group := cut(
     labels = c("0-19", "20-39", "40-59", "60-79", "80-99", "100+"),
     include.lowest = TRUE
 )]
-
-
 dt <- dt[!is.na(age_group)]
+dt[, gender := "female"][SEXO == 1, gender := "male"]
 
-# age categories
-age_vector <- age[, get(paste0("total", sel_year))]
-age_vector <- age_vector / sum(age_vector)
-age_vector <- data.frame(age_group = c(1:21), Freq = as.numeric(age_vector)) # Convert to data.frame)
-age_vector <- data.table(age_vector)[, group := ceiling(.I / 4)][, .(Freq = sum(Freq)), by = group]
+# reshape age categories
+age_vector <- age_vector[, group := ceiling(.I / 4)][, .(Freq = sum(Freq)), by = group]
 age_vector <- cbind(age_group = c("0-19", "20-39", "40-59", "60-79", "80-99", "100+"), age_vector)[, group := NULL]
-# Clean and normalize sex vector
-sex_vector <- sex[, total := NULL][year == sel_year][, year := NULL]
-sex_vector <- sex_vector / sum(sex_vector)
-sex_vector <- data.frame(gender = c("male", "female"), Freq = as.numeric(sex_vector)) # Convert to data.frame
+
 
 # Define raking margins
 margins <- list(
@@ -55,11 +46,31 @@ pop_totals <- list(
 
 # Prepare survey object from dt and set income cuts for quantiles dynamically
 dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$FACTORCAL) # muestra con coeficientes de elevación
-subsample <- subset(dt_sv, CIUDAD == "madrid")
+pre_subsample <- subset(dt_sv, CIUDAD == "madrid")
 
 # Apply raking
-raked_design <- rake(
-    design = subsample,
+subsample <- rake(
+    design = pre_subsample,
     sample.margins = margins,
     population.margins = pop_totals
 )
+
+# Test sample means against true population means using svycontrast
+RNmean <- svymean(~RENTAD, subsample)
+RBmean <- svymean(~RENTAB, subsample)
+
+# Test if the survey means are equal to the population means
+test_rep1 <- svycontrast(RNmean, quote(RENTAD - RNpop)) %>% print()
+test_rep2 <- svycontrast(RBmean, quote(RENTAB - RBpop)) %>% print()
+
+# Summarize the results
+net_vals <- data.table(pop = RNpop,
+ mean = coef(RNmean),
+se = SE(RNmean),
+dif = (RNpop - coef(RNmean)) / RNpop)
+
+gross_vals <- data.table(pop = RBpop,
+ mean = coef(RBmean),
+  se = SE(RBmean),
+  dif = (RBpop - coef(RBmean)) / RBpop)
+results <- rbind(net_vals, gross_vals, use.names = FALSE) %>% print()
