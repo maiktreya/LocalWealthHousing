bdfb77c64abe32ec87593c983c89856a56166825
il    iciuv <miguel.garcia.duch@gmail.com>
2024-10-01 22:39:57 +0200

    last

--- AEAT/src/transform/reweighting.R
+++ AEAT/src/transform/reweighting.R
@@ -9,7 +9,7 @@ rake_data <- function(dt = dt, sel_year = sel_year, city = city) {
     library(survey, quietly = TRUE)
 
     # labels and indexes
-    age_labels <- c("0-19", "20-39", "40-59", "60-79", "80-99", "100+")
+    age_labels <- c("0-19", "20-39", "40-59", "60-79", "80-99+")
     pop_stats <- fread("AEAT/data/pop-stats.csv")
     city_index <- pop_stats[muni == city & year == sel_year, index] %>% as.numeric()
 
