27fe60aebcf87ae8e8b009ae35aaf7a392dbab82
maiktreya <miguel.garcia.duch@gmail.com>
2024-10-02 11:41:01 +0200

    LAST

--- AEAT/src/transform/reweighting.R
+++ AEAT/src/transform/reweighting.R
@@ -58,6 +58,64 @@ rake_data <- function(dt = dt, sel_year = sel_year, city = city) {
 }
 
 
+# Alternative raking function considering interaction between age and sex categories
+
+rake_data <- function(dt = dt, sel_year = sel_year, city = city) {
+    # function dependencies
+    library(data.table, quietly = TRUE)
+    library(survey, quietly = TRUE)
+
+    # labels and indexes
+    age_labels <- c("male_0-19", "male_20-39", "male_40-59", "male_60-79", "male_80-99+",
+                    "female_0-19", "female_20-39", "female_40-59", "female_60-79", "female_80-99+")
+    pop_stats <- fread("AEAT/data/pop-stats.csv")
+    city_index <- pop_stats[muni == city & year == sel_year, index] %>% as.numeric()
+
+    # reshape sex categories
+    sex_vector <- fread(paste0("AEAT/data/", city, "-sex-freq.csv"))[, .(gender, Freq = get(paste0("freq", sel_year)))]
+
+    # reshape age categories
+    age_vector <- fread(paste0("AEAT/data/", city, "-age-freq.csv"))[, .(age_group, Freq = get(paste0("freq", sel_year)))]
+    age_vector <- age_vector[, group := ceiling(.I / 4)][, .(Freq = sum(Freq)), by = group]
+    age_vector <- cbind(age_group = age_labels, age_vector)[, group := NULL]
+
+    # Create a new age_group based on broader 20-year intervals
+    dt[, age_group := cut(
+        AGE,
+        breaks = c(0, 20, 40, 60, 80, Inf), # Defining 20-year groups
+        right = FALSE,
+        labels = age_labels,
+        include.lowest = TRUE
+    )]
+    dt <- dt[!is.na(age_group)]
+    dt <- dt[!is.na(FACTORCAL)]
+    dt[, gender := fifelse(SEXO == 1, "male", "female")]
+    dt[, gender_age := interaction(gender, age_group, sep = "_")]
+
+    # Define raking margins
+    margins <- list(~age_group, ~gender)
+
+    # Population proportions for raking
+    pop_totals <- list(age_vector, sex_vector)
+
+    # Prepare survey object
+    dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$FACTORCAL)
+    pre_subsample <- subset(dt_sv, MUESTRA == city_index)
+
+    # Apply raking for sex and age cohorts
+    subsample <- rake(
+        design = pre_subsample,
+        sample.margins = margins,
+        population.margins = pop_totals
+    )
+
+    dt <- subsample$variables
+    dt[, FACTORCAL := weights(subsample)]
+
+    # return data with new weights
+    return(dt)
+}
+
 # STEP 2: Calibrate for mean income or other known population parameter
 
 calibrate_data <- function(dt = dt, sel_year = sel_year, ref_unit = ref_unit, city = city) {
