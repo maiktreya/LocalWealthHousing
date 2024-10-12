1b60a55d5162e239c6254dc23e999bde6f23bc4a
il    iciuv <miguel.garcia.duch@gmail.com>
2024-10-12 13:01:57 +0200

    last

--- AEAT/src/tests/real_estate.R
+++ AEAT/src/tests/real_estate.R
@@ -7,15 +7,26 @@ source("AEAT/src/transform/etl_pipe.R")
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 dt <- fread("AEAT/data/madridIDENHOG2021.gz")
 
-######
+###### New variables definitions
 
 dt[, NPROP_ALQ2 := as.factor(NPROP_ALQ)]
+dt[RENTA_ALQ2 > 0, RENT_PROP := RENTA_ALQ2 / NPROP_ALQ]
+
+####### Prepare data as survey object
+
 dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$FACTORCAL)
+
+####### Perform calculations to obtain sample statistics
 
 caseros_total <- svytotal(~NPROP_ALQ2, dt_sv) %>%
     prop.table() %>%
-    round(3)
+    round(3) %>%
+    print()
 
 prop_caseros <- svytotal(~NPROP_ALQ2, dt_sv) %>%
     prop.table() %>%
-    round(3)
+    round(3) %>%
+    print()
+rent_prop <- svttotal(~RENT_PROP, dt_sv) %>%
+    round(3) %>%
+    print()
--- AEAT/src/transform/fast_data.R
+++ AEAT/src/transform/fast_data.R
@@ -7,15 +7,11 @@ source("AEAT/src/transform/etl_pipe.R")
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 
 # define city subsample and variables to analyze
-export_object <- FALSE
 city <- "madrid"
 represet <- "!is.na(FACTORCAL)"
 sel_year <- 2021
 ref_unit <- "IDENHOG"
-calib_mode <- TRUE
-city_index <- pop_stats[muni == city & year == sel_year, index]
-RNpop <- pop_stats[muni == city & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
-RBpop <- pop_stats[muni == city & year == sel_year, get(paste0("RB_", tolower(ref_unit)))]
+calibrated <- TRUE
 
 # get a sample weighted for a given city
 dt <- get_wave(
@@ -23,9 +19,11 @@ dt <- get_wave(
     sel_year = sel_year, # wave
     ref_unit = ref_unit, # reference PSU (either household or individual)
     represet = represet, # reference universe/population (whole pop. or tax payers)
-    calibrated = calib_mode, # Weight calib. (TRUE, FALSE, TWO-STEPS) Requieres auxiliary total/mean data
+    calibrated = calibrated, # Weight calib. (TRUE/FALS)E Requieres auxiliary  data
 )
-dt <- subset(dt, MUESTRA == city_index)
+
+# ensure subsample of interest is selected in case calibration is not applied
+dt <- subset(dt, MUESTRA == pop_stats[muni == city & year == sel_year, index])
 
 # export reduced recalibrated matrix
-fwrite(dt, paste0(fwrite(dt, "AEAT/data/", city, ref_unit, sel_year, ".gz")))
+fwrite(dt, paste0("AEAT/data/", city, ref_unit, sel_year, ".gz"))
