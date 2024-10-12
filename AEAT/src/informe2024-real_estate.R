0563f89f7835d3b325a452d63ff989527ac85d75
il    iciuv <miguel.garcia.duch@gmail.com>
2024-10-12 22:45:29 +0200

    last

--- AEAT/src/informe2024-real_estate.R
+++ AEAT/src/informe2024-real_estate.R
@@ -36,11 +36,11 @@ dt[, TIPO_PROP := fcase(
 )]
 dt[, INC_PER_PROP := 0][RENTA_ALQ2 > 0, INC_PER_PROP := RENTA_ALQ2 / NPROP_ALQ]
 
-####### Prepare data as survey object
+# Prepare data as survey object
 
 dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$FACTORCAL)
 
-####### Perform calculations to obtain sample statistics
+# Perform calculations to obtain sample statistics
 
 prop_caseros <- svytable(~TIPO_PROP, dt_sv) %>%
     prop.table() %>%
@@ -52,20 +52,20 @@ conc_rentistas <- svyby(~NPROP_ALQ, ~TIPO_PROP, subset(dt_sv, NPROP_ALQ > 0, ci 
 conc_caseros <- svyby(~RENTA_ALQ2, ~TIPO_PROP, subset(dt_sv, NPROP_ALQ > 0), svytotal)[2] %>%
     prop.table() %>%
     round(3)
-
 ave_income <- svyby(~RENTA_ALQ2, ~TIPO_PROP, subset(dt_sv, RENTA_ALQ2 > 0), svymean)[2] %>%
     round(3) %>%
     print()
 ave_income_per_prop <- svyby(~INC_PER_PROP, ~TIPO_PROP, subset(dt_sv, RENTA_ALQ2 > 0), svymean)[2] %>%
     round(3) %>%
     print()
-
 caseros_total <- svyquantile(~NPROP_ALQ, subset(dt_sv, NPROP_ALQ > 0), quantiles = c(seq(.5, .95, by = .05), .99))$NPROP_ALQ[, 1] %>%
     print()
 rent_prop <- svymean(~RENTA_ALQ2, subset(dt_sv, RENTA_ALQ2 > 0))[1] %>%
     round(3)
 med_rent_prop <- svyquantile(~RENTA_ALQ2, subset(dt_sv, RENTA_ALQ2 > 0), quantiles = .5)$RENTA_ALQ2[, 1]
 props_alq <- svymean(~NPROP_ALQ, subset(dt_sv, NPROP_ALQ > 0))[1]
+
+# Prepare and export results
 
 results <- cbind(prop_caseros[-1], conc_rentistas, conc_caseros, ave_income, ave_income_per_prop)
 pob_inq <- data.table(TIPO_PROP = "no_caseros", N = prop_caseros$N[1], NPROP_ALQ = 0, RENTA_ALQ2 = 0, RENTA_ALQ2 = 0, INC_PER_PROP = 0)
