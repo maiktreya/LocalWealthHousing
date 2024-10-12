efe1920f8be8ec56c06da7727de92f02ed0bc1c7
il    iciuv <miguel.garcia.duch@gmail.com>
2024-10-12 12:42:44 +0200

    last

---
+++ AEAT/src/transform/fast_data.R
@@ -0,0 +1,28 @@
+# clean enviroment and import dependencies
+rm(list = ls())
+library(data.table)
+library(survey)
+library(magrittr)
+source("AEAT/src/transform/etl_pipe.R")
+pop_stats <- fread("AEAT/data/pop-stats.csv")
+
+# define city subsample and variables to analyze
+export_object <- FALSE
+city <- "madrid"
+represet <- "!is.na(FACTORCAL)"
+sel_year <- 2021
+ref_unit <- "IDENHOG"
+calib_mode <- TRUE
+city_index <- pop_stats[muni == city & year == sel_year, index]
+RNpop <- pop_stats[muni == city & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
+RBpop <- pop_stats[muni == city & year == sel_year, get(paste0("RB_", tolower(ref_unit)))]
+
+# get a sample weighted for a given city
+dt <- get_wave(
+    city = city, # subregional unit
+    sel_year = sel_year, # wave
+    ref_unit = ref_unit, # reference PSU (either household or individual)
+    represet = represet, # reference universe/population (whole pop. or tax payers)
+    calibrated = calib_mode, # Weight calib. (TRUE, FALSE, TWO-STEPS) Requieres auxiliary total/mean data
+)
+# dt <- subset(dt, MUESTRA == city_index)
