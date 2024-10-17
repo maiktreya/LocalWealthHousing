62113d4685a0320440766d5cd548c3927e550568
maiktreya <miguel.garcia.duch@gmail.com>
2024-10-17 13:02:03 +0200

    last

--- INE/test_censo.R
+++ INE/test_censo.R
@@ -3,5 +3,4 @@ library(data.table)
 library(magrittr)
 library(survey)
 
-dt1 <- readxl::read_xlsx("INE/censo_2021/Censo_2021.xlsx", sheet = "SgDT") %>% data.table()
-dt2 <- readxl::read_xlsx("INE/censo_2021/Censo_2021.xlsx", sheet = "MadDT") %>% data.table()
+dt <- readxl::read_xlsx("INE/censo_2021/Censo_2021.xlsx", sheet = "SgDT") %>% data.table()
