b544972d912810f2371103150b69a8737fd8609d
maiktreya <miguel.garcia.duch@gmail.com>
2024-10-23 15:50:08 +0200

    last

--- AEAT/src/transform/proxy_tramo.R
+++ AEAT/src/transform/proxy_tramo.R
@@ -8,9 +8,9 @@ pop_stats <- fread("AEAT/data/pop-stats.csv")
 
 # define city subsample and variables to analyze
 export <- "TRUE"
-city <- ""
+city <- "segovia"
 represet <- "!is.na(FACTORCAL)"
-sel_year <- 2016
+sel_year <- 2021
 ref_unit <- "IDENHOG"
 calibrated <- FALSE
 
@@ -20,7 +20,7 @@ dt <- get_wave(
     sel_year = sel_year, # wave
     ref_unit = ref_unit, # reference PSU (either household or individual)
     represet = represet, # reference universe/population (whole pop. or tax payers)
-    calibrated = calibrated, # Weight calib. (TRUE/FALS)E Requieres auxiliary  data
+    calibrated = calibrated # Weight calib. (TRUE/FALS)E Requieres auxiliary  data
 )
 
 # Define weights and create survey object
@@ -28,10 +28,11 @@ dt_sv <- svydesign(
     ~1,
     data = dt,
     weights = dt$FACTORCAL
-) %>% subset(., pop_stats[muni == city & year == sel_year, index]) # ensure subsample of interest is selected in case calibration is not applied
+) %>% subset(CCAA == "7")
+# subset(., pop_stats[muni == city & year == sel_year, index]) # ensure subsample of interest is selected in case calibration is not applied
 
 # Calculate proportion of households by type
-prop_tramo <- svytotal(~TRAMO, dt_sv) %>% data.table()
+prop_tramo <- svytotal(~as.factor(TRAMO), dt_sv) %>% data.table()
 
 # Calculate frequencies and total
 prop_tramo <- data.table(
@@ -39,5 +40,7 @@ prop_tramo <- data.table(
     TOTAL = prop_tramo
 )
 prop_tramo[, index := .I]
-colnames(prop_tramo) <- c("Freq.", "Total", "index")
-prop_tramo2 <- fread(paste0("AEAT/data/tramos-segovia-", sel_year, ".csv"))
+fwrite(prop_tramo, "AEAT/data/tramos-segovia-2021.csv")
+# colnames(prop_tramo) <- c("Freq.", "Total", "index")
+# prop_tramo2 <- fread(paste0("AEAT/data/tramos-segovia-", sel_year, ".csv")) %>% as.factor()
+# cbind(prop_tramo, prop_tramo2) %>% print()
