25ba5e1013a4c2ff2fa62584b2040c9e318421c8
maiktreya <miguel.garcia.duch@gmail.com>
2024-10-23 15:54:46 +0200

    last

--- AEAT/src/transform/proxy_tramo.R
+++ AEAT/src/transform/proxy_tramo.R
@@ -1,5 +1,6 @@
 # clean enviroment and import dependencies
 rm(list = ls())
+gc(full = TRUE, verbose = TRUE)
 library(data.table)
 library(survey)
 library(magrittr)
@@ -7,7 +8,6 @@ source("AEAT/src/transform/etl_pipe.R")
 pop_stats <- fread("AEAT/data/pop-stats.csv")
 
 # define city subsample and variables to analyze
-export <- "TRUE"
 city <- "segovia"
 represet <- "!is.na(FACTORCAL)"
 sel_year <- 2021
@@ -28,8 +28,7 @@ dt_sv <- svydesign(
     ~1,
     data = dt,
     weights = dt$FACTORCAL
-) %>% subset(CCAA == "7")
-# subset(., pop_stats[muni == city & year == sel_year, index]) # ensure subsample of interest is selected in case calibration is not applied
+) %>% subset(pop_stats[muni == city & year == sel_year, index]) # ensure subsample of interest is selected in case calibration is not applied
 
 # Calculate proportion of households by type
 prop_tramo <- svytotal(~as.factor(TRAMO), dt_sv) %>% data.table()
@@ -40,7 +39,6 @@ prop_tramo <- data.table(
     TOTAL = prop_tramo
 )
 prop_tramo[, index := .I]
-fwrite(prop_tramo, "AEAT/data/tramos-segovia-2021.csv")
-# colnames(prop_tramo) <- c("Freq.", "Total", "index")
-# prop_tramo2 <- fread(paste0("AEAT/data/tramos-segovia-", sel_year, ".csv")) %>% as.factor()
-# cbind(prop_tramo, prop_tramo2) %>% print()
+colnames(prop_tramo) <- c("Freq.", "Total", "index")
+prop_tramo2 <- fread(paste0("AEAT/data/tramos-segovia-", sel_year, ".csv")) %>% as.factor()
+cbind(prop_tramo$Freq., prop_tramo2$Freq.) %>% print()
