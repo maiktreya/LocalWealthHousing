ee8f774159e65e694f6a1b5c908855e3fb2c2063
maiktreya <miguel.garcia.duch@gmail.com>
2024-11-14 15:43:39 +0100

    last

--- INE/src/tests-repres_typehousehold.R
+++ INE/src/tests-repres_typehousehold.R
@@ -10,9 +10,9 @@ tipos_cat <- fread("AEAT/data/tipohog-madrid-2016.csv")[, .(Desc, Tipohog, index
 
 # define city subsample and variables to analyze
 export_object <- FALSE
-city <- "segovia"
+city <- "madrid"
 represet <- "!is.na(FACTORCAL)"
-sel_year <- 2021
+sel_year <- 2016
 ref_unit <- "IDENHOG"
 calib_mode <- FALSE
 RNpop <- pop_stats[muni == city & year == sel_year, get(paste0("RN_", tolower(ref_unit)))]
@@ -28,14 +28,26 @@ dt <- get_wave(
 )
 dt[, TIPOHOG1 := fcase(
     default = NA,
-    TIPOHOG == 1, 1,
-    TIPOHOG == 2, 2,
-    TIPOHOG %in% c(3, 4, 5, 6), 3,
-    TIPOHOG %in% c(7, 8), 4,
-    TIPOHOG %in% c(9, 10), 5
-)]
+    TIPOHOG == "1.1.1", 1,
+    TIPOHOG == "1.1.2", 2,
+    TIPOHOG %in% c("1.2", "2.1.1", "2.1.2", "2.2.1", "2.1.3"), 3,
+    TIPOHOG %in% c("2.2.1", "2.2.2"), 4,
+    TIPOHOG %in% c("2.3.1", "2.3.2"), 5
+)][, TIPOHOG1 := as.factor(TIPOHOG1)]
 
 
 dt_sv <- svydesign(ids = ~IDENHOG, data = dt, weights = dt$FACTORCAL) %>% subset(MUESTRA == pop_stats[muni == city & year == sel_year, index])
 
-svytotal(~TIPOHOG1, dt_sv) %>% data.table() %>% print()
+new_tipohog_names <- fread(paste0("AEAT/data/tipohog-segovia-", sel_year, "-reduced.csv"))[, .(index, Tipohog, Desc)]
+
+new_tipohog_tot <- svytotal(~TIPOHOG1, dt_sv) %>%
+    as.numeric() %>%
+    print()
+
+new_tipohog_freq <- svytotal(~TIPOHOG1, dt_sv) %>%
+    prop.table() %>%
+    as.numeric()
+
+new_tipohog <- data.table(new_tipohog_names, Freq = new_tipohog_freq, Total = new_tipohog_tot)
+
+fwrite(new_tipohog, paste0("AEAT/data/tipohog-", city, "-", sel_year, "-reduced.csv"))
