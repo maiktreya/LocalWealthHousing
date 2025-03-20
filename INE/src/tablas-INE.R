b03be76cd6b00e48a17b8bd58f1279bbb75a88e7
maiktreya <miguel.garcia.duch@gmail.com>
2025-03-20 10:42:11 +0100

    Refactor data retrieval process in tablas-INE.R to improve clarity and organization

--- INE/src/tablas-INE.R
+++ INE/src/tablas-INE.R
@@ -1,6 +1,12 @@
+# Listado de tablas base utilizadas para elaborar el informe del alquiler de la ciudad de Segovia
 # import data.table and magrittr for data handling
 library(data.table)
 library(magrittr)
+
+# clean and prepare enviroment
+rm(list = ls())
+gc(verbose = TRUE)
+full_export <- FALSE
 
 # definir una función para lanzar requests a la API del INE
 get_ine <- function(id_tabla = NA, municipio = NA) {
@@ -12,45 +18,47 @@ get_ine <- function(id_tabla = NA, municipio = NA) {
     return(dt)
 }
 
-# base de datos de ventas de vivienda por municipios (Min transporte y sost)
-download.file("https://apps.fomento.gob.es/BoletinOnline2/sedal/34010210.XLS",
-    destfile = "pventa.xls",
-    mode = "wb"
-)
+if (full_export) {
+    # base de datos de ventas de vivienda por municipios (Min transporte y sost)
+    download.file("https://apps.fomento.gob.es/BoletinOnline2/sedal/34010210.XLS",
+        destfile = "pventa.xls",
+        mode = "wb"
+    )
+}
 
 # Plazas, apartamentos, grados de ocupación y personal empleado por puntos turísticos (Encuesta de ocupación en Alojamientos Turísticos extra-hoteleros)
-dt <- get_ine(2083, "Segovia")
+dt1 <- get_ine(2083, "Segovia")
 
 # Viajeros y pernoctaciones por puntos turísticos (Encuesta de ocupación hotelera)
-dt <- get_ine(2078, "Segovia")
+dt2 <- get_ine(2078, "Segovia")
 
 # Establecimientos, plazas estimadas, grados de ocupación y personal empleado por puntos turísticos (Encuesta de ocupación hotelera)
-dt <- get_ine(2076, "Segovia")
+dt3 <- get_ine(2076, "Segovia")
 
 # Ocupación en alojamientos turísticos (Estadística Experimental)
-dt <- get_ine(48427, "Segovia")
+dt4 <- get_ine(48427, "Segovia")
 
 # Medición del número de viviendas turísticas en España y su capacidad (Estadística Experimental)
-dt <- get_ine(39366, "Segovia")
+dt5 <- get_ine(39366, "Segovia")
 
 # Viviendas familiares convencionales según tipo de vivienda y año de construcción (agregado) del edificio (Censo 2021)
-dt <- get_ine(59527, "Segovia")
+dt6 <- get_ine(59527, "Segovia")
 
 # Viviendas familiares convencionales según tipo de vivienda y superficie (Censo 2021)
-dt <- get_ine(59528, "Segovia")
+dt7 <- get_ine(59528, "Segovia")
 
 # Viviendas familiares principales convencionales según régimen de tenencia (Censo 2021)
-dt <- get_ine(59529, "Segovia")
+dt8 <- get_ine(59529, "Segovia")
 
 # Hogares según su tamaño por estructura del hogar (Censo 2021)
-dt <- get_ine(59945, "Segovia")
+dt9 <- get_ine(59945, "Segovia")
 
 # Índice de Precios de la Vivienda en Alquiler (IPVA). Base 2015
-dt <- get_ine(59060, "Segovia")
+dt10 <- get_ine(59060, "Segovia")
 
 # Índice de Precios de la Vivienda en Alquiler (IPVA) por distritos. Base 2015
-dt <- get_ine(59061, "Segovia")
+dt11 <- get_ine(59061, "Segovia")
+
+dt <- list(dt1, dt2, dt3, dt4, dt5, dt6, dt7, dt8, dt9, dt10, dt11)
 
 dt %>% print()
-
-
